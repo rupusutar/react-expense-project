c50469bcf6ba03d01f7c60990048c7139fde490d
Rupali Sutar <rupusutar@gmail.com>
2022-04-04 23:41:14 +0100

    add new expenses to initial expense list

--- src/App.js
+++ src/App.js
@@ -1,37 +1,40 @@
 import Expenses from "./components/ExpensesList/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
+import { useState } from "react";
+
+const INITIAL_DATA = [
+	{
+		id: "e1",
+		title: "Toilet Paper",
+		amount: 94.12,
+		date: new Date(2020, 7, 14),
+	},
+	{ id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+	{
+		id: "e3",
+		title: "Car Insurance",
+		amount: 294.67,
+		date: new Date(2021, 2, 28),
+	},
+	{
+		id: "e4",
+		title: "New Desk (Wooden)",
+		amount: 450,
+		date: new Date(2021, 5, 12),
+	},
+];
 
 function App() {
-	const data = [
-		{
-			id: "e1",
-			title: "Toilet Paper",
-			amount: 94.12,
-			date: new Date(2020, 7, 14),
-		},
-		{ id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-		{
-			id: "e3",
-			title: "Car Insurance",
-			amount: 294.67,
-			date: new Date(2021, 2, 28),
-		},
-		{
-			id: "e4",
-			title: "New Desk (Wooden)",
-			amount: 450,
-			date: new Date(2021, 5, 12),
-		},
-	];
+	const [expenses, setExpenses] = useState(INITIAL_DATA);
 
 	const newExpenseHandler = (newExpense) => {
-		console.log(newExpense); //TODO store all data in separate file
+		setExpenses((prevExpenses) => [newExpense, ...prevExpenses]);
 	};
 
 	return (
 		<div>
 			<NewExpense onSave={newExpenseHandler} />
-			<Expenses data={data} />
+			<Expenses data={expenses} />
 		</div>
 	);
 }
--- src/components/ExpensesList/Expenses.js
+++ src/components/ExpensesList/Expenses.js
@@ -1,10 +1,22 @@
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
+import ExpenseFilter from "./ExpenseFilter";
+import { useState } from "react";
 
 export default function Expenses({ data }) {
+	const [selectedYear, setSelectedYear] = useState("2022");
+
+	const fliterChangeHandler = (newYearFilter) => {
+		setSelectedYear(newYearFilter);
+	};
+
 	return (
 		<Card className="expenses" testid="expenses1">
+			<ExpenseFilter
+				selectedYear={selectedYear}
+				onYearChange={fliterChangeHandler}
+			></ExpenseFilter>
 			{data.map((expense) => (
 				<ExpenseItem
 					key={expense.id}
